2720c55eb4100e38ed60cbdd3cc25421d2d94358
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-23 12:22:25 +0700

    05. Import example data and displaying the list of objects

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO Persons(name,lastname,programming_language) VALUES("Muhammad","Iffat","Java");
+INSERT INTO Persons(name,lastname,programming_language) VALUES("Faqih","Nurul Fahmi","Javascript");
